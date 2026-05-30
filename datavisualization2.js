5ca5fe879002228bb54f6833076c37e164f965bf
dylanp427 <dylanp427@gmail.com>
2026-05-30 23:41:14 +1000

    Initial commit of space debris visualisation project

---
+++ datavisualization2.js
@@ -0,0 +1,48 @@
+var vg_1 = "Chart1.json";
+vegaEmbed("#c", vg_1).then(function(result) {
+
+}).catch(console.error);
+var vg_2 = "Chart2.json";
+vegaEmbed("#c2", vg_2).then(function(result) {
+
+}).catch(console.error);
+
+var vg_3 = "Chart3.json";
+vegaEmbed("#c3", vg_3).then(function(result) {
+
+}).catch(console.error);
+
+var vg_4 = "Chart4.json";
+vegaEmbed("#c4", vg_4).then(function(result) {
+
+}).catch(console.error);
+
+
+var vg_5 = "Chart5.json";
+vegaEmbed("#c5", vg_5).then(function(result) {
+
+}).catch(console.error);
+
+var vg_6 = "Chart6.json";
+vegaEmbed("#c6", vg_6).then(function(result) {
+
+}).catch(console.error);
+
+var vg_7 = "Chart7.json";
+vegaEmbed("#c7", vg_7).then(function(result) {
+
+}).catch(console.error);
+var vg_8 = "Chart8.json";
+vegaEmbed("#c8", vg_8).then(function(result) {
+
+}).catch(console.error);
+var vg_9 = "Chart9.json";
+vegaEmbed("#c9", vg_9   ).then(function(result) {
+
+}).catch(console.error);
+
+var vg_10 = "Chart10.json";
+vegaEmbed("#c10", vg_10).then(function(result) {
+    
+
+}).catch(console.error);
